306b5b966e3750bdcd28ea37f244ec2ea5167cb6
Rümeysa Karagöz <rumeysa.karagoz@turk.net>
2023-10-01 10:02:48 +0300

    added layout dir

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,59 +1,20 @@
-import { useState } from "react"
-import Product from "../components/Products/product"
-import styles from "../styles/Home.module.css"
+// import "globals.css"
 import Navbar from "../components/navbar"
+import Head from "next/head"
+import Products from "../components/Products/products"
 
 export default function Home(data) {
   let products = Object.entries(data?.data)
 
   console.log("products", products)
 
-  const deleteProduct = (id) => {
-    fetch(`https://fakestoreapi.com/products/${id}`)
-      .then((res) => res.json())
-      .then((json) => {
-        products = products.filter((i) => i[1].id !== json.id)
-        console.log(products)
-      })
-  }
-  const changeOrder = () => {
-    fetch("https://fakestoreapi.com/products?sort=desc")
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json)
-        let products = res.data
-      })
-  }
-
   return (
     <>
+      <Head>
+        <title>My page title</title>
+      </Head>
       <Navbar />
-      <div className={styles.sortingElement}>
-        <div className={styles.dropdown}>
-          <p>Sıralama Ölçütü</p>
-        </div>
-        <div className={styles.dropdownContent}>
-          <p>Artan Sıralama</p>
-          <p>Azalan Sıralama</p>
-        </div>
-      </div>
-      <div onClick={() => changeOrder()}></div>
-
-      <div className={styles.container}>
-        {!products ? (
-          <div>load</div>
-        ) : (
-          products.map((item) => {
-            return (
-              <Product
-                key={item[0]}
-                product={item[1]}
-                onDeleteProduct={deleteProduct}
-              />
-            )
-          })
-        )}
-      </div>
+      <Products data={products} />
     </>
   )
 }
